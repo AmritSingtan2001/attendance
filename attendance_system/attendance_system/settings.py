a6fa14dddc7f183de8df2a3227e01288fc8de758
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-09-22 10:05:59 +0545

    refactor:setting file

--- attendance_system/attendance_system/settings.py
+++ attendance_system/attendance_system/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 if DEBUG:
     ALLOWED_HOSTS = ['192.168.18.11','127.0.0.1','192.168.18.97']
